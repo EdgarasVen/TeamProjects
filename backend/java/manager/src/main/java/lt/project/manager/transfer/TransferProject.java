37346a5b0dd06dff71a4ab4c311058fa9a9e793e
EdgarasVen <55981137+EdgarasVen@users.noreply.github.com>
2020-04-08 09:42:15 +0300

    polished transferProject

--- backend/java/manager/src/main/java/lt/project/manager/transfer/TransferProject.java
+++ backend/java/manager/src/main/java/lt/project/manager/transfer/TransferProject.java
@@ -1,16 +1,17 @@
 package lt.project.manager.transfer;
 
+import lt.project.manager.enums.Status;
 import lt.project.manager.model.Project;
 
 public class TransferProject {
 
     private String name;
     private String description;
-    private String companyName;
+    private Status status;
 
     public Project build(){
         return new Project(name,
                 description,
-                "UAB "+companyName);
+                status);
     }
 }

605fbb2e667c5a15678866b43ba6192a7348c8be
EdgarasVen <55981137+EdgarasVen@users.noreply.github.com>
2020-04-10 13:13:45 +0300

    delete api fix url path

--- backend/java/manager/src/main/java/lt/project/manager/controller/DeleteApi.java
+++ backend/java/manager/src/main/java/lt/project/manager/controller/DeleteApi.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 public class DeleteApi extends ApiController{
 
-    @DeleteMapping(".api/project/{name}")
+    @DeleteMapping("api/project/{name}")
     public void deleteProjectByName(@PathVariable String name){
         repository.deleteProjectByName(name);
     }
     
-    @DeleteMapping(".api/task/{id}")
+    @DeleteMapping("api/task/{id}")
     public void deleteTaskById(@PathVariable Long id){
         repository.deleteTaskById(id);
         
